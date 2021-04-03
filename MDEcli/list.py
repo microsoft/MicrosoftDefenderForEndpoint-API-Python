6f2cc458fc77ee44981d806915ac4fd4f1c78373
bmrodrigues <rodrigues.bruno@microsoft.com>
2021-04-03 16:17:59 +0100

    added MA and improvements in main

--- MDEcli/list.py
+++ MDEcli/list.py
@@ -27,18 +27,42 @@ class List:
             req = urllib.request.Request(url=url, headers=headers)
             response = urllib.request.urlopen(req)
             jsonResponse = json.loads(response.read())
-            
-            # CSV
-                        
-            fields = jsonResponse['value'][0].keys()
 
-            with open(self.filename, 'w', newline='') as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames = fields)
-                writer.writeheader()
-                writer.writerows(jsonResponse['value'])
+            # Empty results
+            if not jsonResponse['value']:
+                print('No automated investigations found.')
             
-            for index, item in enumerate(jsonResponse['value']):
-                print('\n')
-                print(item)
-            
-            print(f'\nFull results can be imported to PowerBI from {self.filename}\n')
+            else:
+                # CSV & print        
+                fields = jsonResponse['value'][0].keys()
+
+                with open(self.filename, 'w', newline='') as csvfile:
+                    writer = csv.DictWriter(csvfile, fieldnames = fields)
+                    writer.writeheader()
+                    writer.writerows(jsonResponse['value'])
+                
+                for index, item in enumerate(jsonResponse['value']):
+                    print('\n')
+                    print(item)
+                
+                print(f'\nFull results can be imported to PowerBI from {self.filename}\n')
+    
+    def list_no_csv(self):
+        login_MDEcli = login.Login(self.tenantId, self.appId, self.appSecret)
+        aadToken = login_MDEcli.login()
+
+        url = f"https://api.securitycenter.microsoft.com/api/{self.urlpart}"
+        headers = { 
+            'Content-Type' : 'application/json',
+            'Accept' : 'application/json',
+            'Authorization' : "Bearer " + aadToken
+        }
+
+        req = urllib.request.Request(url=url, headers=headers)
+        response = urllib.request.urlopen(req)
+        jsonResponse = json.loads(response.read())
+        
+        print('\n')
+        for item in jsonResponse.items():
+            print(item)
+        print('\n')
--- MDEcli/main.py
+++ MDEcli/main.py
@@ -2,6 +2,7 @@ import sys
 import getpass
 import argparse
 import list
+import actions
 
 # Menu
 parser = argparse.ArgumentParser()
@@ -19,6 +20,16 @@ machinesparser.add_argument('--list', help='Retrieves a collection of Machines t
 #Machines Actions menu
 machineactionsparser = subparsers.add_parser('machine-actions', help='MachineAction resource type')
 machineactionsparser.add_argument('--list', help='Retrieves a collection of Machine Actions', action='store_true')
+machineactionsparser.add_argument('--collect', help='Collect investigation package from a device.', action='store_true')
+machineactionsparser.add_argument('--full-isolate', dest='full_isolate', help='Fully isolates a device from accessing external network.', action='store_true')
+machineactionsparser.add_argument('--selective-isolate', dest='selective_isolate', help='Restrict only limited set of applications from accessing the network.', action='store_true')
+machineactionsparser.add_argument('--unisolate', help='Undo isolation of a device.', action='store_true')
+machineactionsparser.add_argument('--restrict-app-execution', dest='restrict_app_execution', help='Restrict execution of all applications on the device except a predefined set.', action='store_true')
+machineactionsparser.add_argument('--unrestrict-app-execution', dest='unrestrict_app_execution', help='Enable execution of any application on the device.', action='store_true')
+machineactionsparser.add_argument('--quick', help='Initiate Microsoft Defender Antivirus quick scan on a device.', action='store_true')
+machineactionsparser.add_argument('--full', help='Initiate Microsoft Defender Antivirus full scan on a device.', action='store_true')
+machineactionsparser.add_argument('--offboard', help='Offboard device from Defender for Endpoint.', action='store_true')
+machineactionsparser.add_argument('--quarantine', help='Stop execution of a file on a device and delete it.', action='store_true')
 
 #Automated Investigations menu
 automatedinvestigationsparser = subparsers.add_parser('automated-investigations', help='Investigation resource type')
@@ -79,10 +90,136 @@ elif args.Commands == 'machine-actions':
         appSecret = getpass.getpass('Enter your application Secret (does not show in cli): ')
         list_machine_actions = list.List(tenantId, appId, appSecret, 'machineactions', 'machineactions.csv')
         list_machine_actions.list()
+    elif args.collect:
+        tenantId = input('Enter your Tenant ID: ')
+        appId = input('Enter your application ID: ')
+        appSecret = getpass.getpass('Enter your application Secret (does not show in cli): ')
+        comment = input('Comment: ')
+        av_body = {
+                        'Comment' : comment
+                    }
+        filename = input('CSV filename with machine IDs: ')
+        column = input('Column with machine IDs: ')
+        action_collect = actions.Action(tenantId, appId, appSecret, av_body, 'collectInvestigationPackage', filename, column)
+        action_collect.action()
+    elif args.full_isolate:
+        tenantId = input('Enter your Tenant ID: ')
+        appId = input('Enter your application ID: ')
+        appSecret = getpass.getpass('Enter your application Secret (does not show in cli): ')
+        comment = input('Comment: ')
+        body = {
+                        'Comment' : comment,
+                        'IsolationType' : 'Full'
+                    }
+        filename = input('CSV filename with machine IDs: ')
+        column = input('Column with machine IDs: ')
+        action_full_isolate = actions.Action(tenantId, appId, appSecret, body, 'isolate', filename, column)
+        action_full_isolate.action()
+    elif args.selective_isolate:
+        tenantId = input('Enter your Tenant ID: ')
+        appId = input('Enter your application ID: ')
+        appSecret = getpass.getpass('Enter your application Secret (does not show in cli): ')
+        comment = input('Comment: ')
+        body = {
+                        'Comment' : comment,
+                        'IsolationType' : 'Selective'
+                    }
+        filename = input('CSV filename with machine IDs: ')
+        column = input('Column with machine IDs: ')
+        action_selective_isolate = actions.Action(tenantId, appId, appSecret, body, 'isolate', filename, column)
+        action_selective_isolate.action()
+    elif args.unisolate:
+        tenantId = input('Enter your Tenant ID: ')
+        appId = input('Enter your application ID: ')
+        appSecret = getpass.getpass('Enter your application Secret (does not show in cli): ')
+        comment = input('Comment: ')
+        body = {
+                        'Comment' : comment,
+                    }
+        filename = input('CSV filename with machine IDs: ')
+        column = input('Column with machine IDs: ')
+        action_unisolate = actions.Action(tenantId, appId, appSecret, body, 'unisolate', filename, column)
+        action_unisolate.action()
+    elif args.restrict_app_execution:
+        tenantId = input('Enter your Tenant ID: ')
+        appId = input('Enter your application ID: ')
+        appSecret = getpass.getpass('Enter your application Secret (does not show in cli): ')
+        comment = input('Comment: ')
+        body = {
+                        'Comment' : comment,
+                    }
+        filename = input('CSV filename with machine IDs: ')
+        column = input('Column with machine IDs: ')
+        action_restrictCodeExecution = actions.Action(tenantId, appId, appSecret, body, 'restrictCodeExecution', filename, column)
+        action_restrictCodeExecution.action()
+    elif args.unrestrict_app_execution:
+        tenantId = input('Enter your Tenant ID: ')
+        appId = input('Enter your application ID: ')
+        appSecret = getpass.getpass('Enter your application Secret (does not show in cli): ')
+        comment = input('Comment: ')
+        body = {
+                        'Comment' : comment,
+                    }
+        filename = input('CSV filename with machine IDs: ')
+        column = input('Column with machine IDs: ')
+        action_unrestrictCodeExecution = actions.Action(tenantId, appId, appSecret, body, 'unrestrictCodeExecution', filename, column)
+        action_unrestrictCodeExecution.action()
+    elif args.quick:
+        tenantId = input('Enter your Tenant ID: ')
+        appId = input('Enter your application ID: ')
+        appSecret = getpass.getpass('Enter your application Secret (does not show in cli): ')
+        comment = input('Comment: ')
+        body = {
+                        'Comment' : comment,
+                        'ScanType' : 'Quick'
+                    }
+        filename = input('CSV filename with machine IDs: ')
+        column = input('Column with machine IDs: ')
+        action_quick_scan = actions.Action(tenantId, appId, appSecret, body, 'runAntiVirusScan', filename, column)
+        action_quick_scan.action()
+    elif args.full:
+        tenantId = input('Enter your Tenant ID: ')
+        appId = input('Enter your application ID: ')
+        appSecret = getpass.getpass('Enter your application Secret (does not show in cli): ')
+        comment = input('Comment: ')
+        body = {
+                        'Comment' : comment,
+                        'ScanType' : 'Full'
+                    }
+        filename = input('CSV filename with machine IDs: ')
+        column = input('Column with machine IDs: ')
+        action_full_scan = actions.Action(tenantId, appId, appSecret, body, 'runAntiVirusScan', filename, column)
+        action_full_scan.action()
+    elif args.offboard:
+        tenantId = input('Enter your Tenant ID: ')
+        appId = input('Enter your application ID: ')
+        appSecret = getpass.getpass('Enter your application Secret (does not show in cli): ')
+        comment = input('Comment: ')
+        body = {
+                        'Comment' : comment
+                    }
+        filename = input('CSV filename with machine IDs: ')
+        column = input('Column with machine IDs: ')
+        action_offboard = actions.Action(tenantId, appId, appSecret, body, 'offboard', filename, column)
+        action_offboard.action()
+    elif args.quarantine:
+        tenantId = input('Enter your Tenant ID: ')
+        appId = input('Enter your application ID: ')
+        appSecret = getpass.getpass('Enter your application Secret (does not show in cli): ')
+        comment = input('Comment: ')
+        sha1 = input('Insert SHA1 of the filename to stop and quarantine: ')
+        body = {
+                        'Comment' : comment,
+                        'Sha1' : sha1
+                    }
+        filename = input('CSV filename with machine IDs: ')
+        column = input('Column with machine IDs: ')
+        action_quarantine = actions.Action(tenantId, appId, appSecret, body, 'StopAndQuarantineFile', filename, column)
+        action_quarantine.action()
     else:
         parser.print_help(sys.stderr)
 
-# Automated Investigations --> requires checking
+# Automated Investigations
 elif args.Commands == 'automated-investigations':
     if args.list:
         tenantId = input('Enter your Tenant ID: ')
@@ -104,14 +241,14 @@ elif args.Commands == 'indicators':
     else:
         parser.print_help(sys.stderr)
 
-# Score --> requires checking
+# Score
 elif args.Commands == 'score':
     if args.list:
         tenantId = input('Enter your Tenant ID: ')
         appId = input('Enter your application ID: ')
         appSecret = getpass.getpass('Enter your application Secret (does not show in cli): ')
-        list_score = list.List(tenantId, appId, appSecret, 'exposureScore', 'exposureScore.csv')
-        list_score.list()
+        list_score = list.List(tenantId, appId, appSecret, 'exposureScore')
+        list_score.list_no_csv()
     else:
         parser.print_help(sys.stderr)
 
@@ -126,16 +263,16 @@ elif args.Commands == 'software':
     else:
         parser.print_help(sys.stderr)
 
-# Vulnerabilities --> NOT WORKING RETURNS ALL CVE's
-#elif args.Commands == 'vulnerabilities':
-#    if args.list:
-#        tenantId = input('Enter your Tenant ID: ')
-#        appId = input('Enter your application ID: ')
-#        appSecret = getpass.getpass('Enter your application Secret (does not show in cli): ')
-#        list_vulnerabilities = list.List(tenantId, appId, appSecret, 'vulnerabilities', 'vulnerabilities.csv')
-#        list_vulnerabilities.list()
-#    else:
-#        parser.print_help(sys.stderr)
+# Vulnerabilities --> NOT WORKING RETURNS ALL CVE's - ICM openend https://portal.microsofticm.com/imp/v3/incidents/details/234066458/home
+elif args.Commands == 'vulnerabilities':
+    if args.list:
+        tenantId = input('Enter your Tenant ID: ')
+        appId = input('Enter your application ID: ')
+        appSecret = getpass.getpass('Enter your application Secret (does not show in cli): ')
+        list_vulnerabilities = list.List(tenantId, appId, appSecret, 'vulnerabilities', 'vulnerabilities.csv')
+        list_vulnerabilities.list()
+    else:
+        parser.print_help(sys.stderr)
 
 # Recomendations
 elif args.Commands == 'recomendations':
